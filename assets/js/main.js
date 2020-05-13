e6a9d4c1b3b1872081d8ffef4cd8fdd9b7fbbc08
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-05-13 13:49:45 +0900

    説明文フェードイン追記

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,19 @@ $(function(){
         });
     });
 });
+
+$(function(){
+    $('.description').addClass('move');
+    $(window).scroll(function(){
+      $(".description").each(function(){
+        var imgPos = $(this).offset().top;    
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + windowHeight/5){
+          $(this).find("p").removeClass('move');
+        } else {
+          $(this).find("p").addClass('move');
+        }
+      });
+    });
+  });
